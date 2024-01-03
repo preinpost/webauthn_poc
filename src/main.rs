a593c6ef27e9a75a7c0442775305e8174451a620
minsoo.lee <preinpost@gmail.com>
2024-01-03 23:03:01 +0900

    login 추가

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,8 @@ use base64::Engine;
 use base64::engine::general_purpose;
 use log::info;
 use ring::rand::{SecureRandom, SystemRandom};
+use sqlx::{Executor, FromRow, Pool, Sqlite};
+use sqlx::sqlite::SqlitePoolOptions;
 use webauthn_rs::prelude::*;
 
 use crate::session::MemorySession;
@@ -71,7 +73,12 @@ async fn get_challenge(webauthn: Data<Webauthn>, session: Session) -> Json<Creat
 }
 
 #[post("member_register")]
-async fn member_register(req: Json<RegisterPublicKeyCredential>, webauthn: Data<Webauthn>, session: Session) -> HttpResponse {
+async fn member_register(
+    req: Json<RegisterPublicKeyCredential>,
+    webauthn: Data<Webauthn>,
+    session: Session,
+    pool: Data<Pool<Sqlite>>
+) -> HttpResponse {
     // info!("req = {:?}", &req);
     let reg_state = session.get::<PasskeyRegistration>("reg_state").unwrap().unwrap();
     // info!("after reg_state = {:?}", &reg_state);
@@ -84,9 +91,66 @@ async fn member_register(req: Json<RegisterPublicKeyCredential>, webauthn: Data<
         })
         .expect("error");
 
+    let ser = general_purpose::URL_SAFE_NO_PAD.encode(serde_json::to_string(&sk).unwrap());
+    info!("ser = {:?}", &ser);
+
+    let cnt = sqlx::query("INSERT INTO users (user_id, name, passkey) VALUES ($1, $2, $3)")
+        .bind("user1")
+        .bind("John Doe")
+        .bind(ser)
+        .execute(pool.get_ref()).await.unwrap();
+
+    info!("cnt = {:?}", cnt);
+
     session.insert("key", sk.clone());
 
     HttpResponse::Ok().finish()
+}
+
+#[get("get_login_challenge")]
+async fn get_login_challenge(webauthn: Data<Webauthn>, session: Session, pool: Data<Pool<Sqlite>>) -> Json<RequestChallengeResponse> {
+
+    let user = sqlx::query_as::<_, User>("SELECT * FROM users WHERE user_id = 'user1'")
+        .fetch_one(pool.get_ref()).await.unwrap();
+
+    info!("user.user_id = {:?}", user.user_id);
+    info!("user.passkey = {:?}", user.passkey);
+
+    let passkey = general_purpose::URL_SAFE_NO_PAD.decode(user.passkey.as_bytes()).unwrap();
+    info!("passkey = {:?}", passkey);
+    let passkey: Passkey = serde_json::from_str(String::from_utf8(passkey).unwrap().as_str()).unwrap();
+
+    let (rcr, auth_state) = webauthn.start_passkey_authentication(&*vec![passkey])
+        .expect("Failed to start login.");
+
+    session.insert("auth_state", auth_state).expect("fail to insert auth_state");
+
+    Json(rcr)
+}
+
+#[post("member_login")]
+async fn member_login(
+    req: Json<PublicKeyCredential>,
+    webauthn: Data<Webauthn>,
+    session: Session,
+    pool: Data<Pool<Sqlite>>
+) -> impl Responder {
+
+    let auth_state = session.get::<PasskeyAuthentication>("auth_state").unwrap().unwrap();
+
+    let auth_result = webauthn
+        .finish_passkey_authentication(&req, &auth_state)
+        .map_err(|e| {
+            info!("challenge_register -> {:?}", e);
+            return HttpResponse::BadRequest().finish();
+        })
+        .expect("error");
+
+
+    info!("Authentication Successful!");
+
+
+    HttpResponse::Ok().body("member_login")
 }
 
 async fn get_htmx(req: HttpRequest, resource_path: web::Path<String>) -> HttpResponse {
@@ -98,7 +162,6 @@ async fn get_htmx(req: HttpRequest, resource_path: web::Path<String>) -> HttpRes
         HttpResponse::NotFound().body("404")
     }
 }
-
 
 fn generate_rand() -> String {
     let mut rand_key = vec![0; 64];
@@ -119,6 +182,8 @@ async fn main() {
     type UserData = HashMap<String, String>;
     let webauthn_users = Data::new(Mutex::new(UserData::new()));
 
+    let pool = sqlx_sqlite_init().await.unwrap();
+
     // cookie key
     let key = Key::generate();
 
@@ -135,11 +200,14 @@ async fn main() {
             .app_data(JsonConfig::default())
             .app_data(create_webauthn())
             .app_data(webauthn_users.clone())
+            .app_data(Data::new(pool.clone()).clone())
             .service(
                 web::scope("/login")
                     .service(web_idx)
                     .service(get_challenge)
                     .service(member_register)
+                    .service(get_login_challenge)
+                    .service(member_login)
             )
         // .service(web::resource("/web/{resource_path}").route(web::get().to(get_htmx)))
     })
@@ -149,6 +217,33 @@ async fn main() {
         .await
         .unwrap();
 }
+
+
+async fn sqlx_sqlite_init() -> sqlx::Result<(Pool<Sqlite>)> {
+    let pool = SqlitePoolOptions::new()
+        .max_connections(5)
+        .connect("sqlite::memory:").await?;
+
+    // create users table
+    sqlx::query("CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id TEXT NOT NULL,
+            name TEXT NOT NULL,
+            passkey TEXT NULLABLE
+        )").execute(&pool).await?;
+
+    Ok(pool)
+}
+
+#[derive(Debug, FromRow)]
+pub struct User {
+    pub id: i64,
+    pub user_id: String,
+    pub name: String,
+    pub passkey: String
+}
+
+
 
 #[cfg(test)]
 mod tests {
@@ -186,50 +281,6 @@ mod tests {
 
         let public_key: RsaPublicKey = private_key.clone().into();
         println!("{:?}", public_key);
-    }
-
-    #[test]
-    fn read_private_key() {
-        let pkcs8_bytes = include_bytes!("../passkey_test.pk8");
-        let key_pair = RsaKeyPair::from_pkcs8(pkcs8_bytes).expect("Failed to parse PKCS#8 key");
-
-        // 공개 키 가져오기
-        let public_key_bytes = key_pair.public_key().as_ref();
-        let public_key_base64 = general_purpose::STANDARD_NO_PAD.encode(key_pair.public_key().as_ref());
-
-        // println!("Public Key: {:?}", public_key_bytes);
-
-        println!("Public Key BASE64: {:?}", public_key_base64);
-    }
-
-
-    #[test]
-    fn test_sign() {
-        let pkcs8_bytes = include_bytes!("../passkey_test.pk8");
-        let key_pair = RsaKeyPair::from_pkcs8(pkcs8_bytes).expect("Failed to parse PKCS#8 key");
-
-        // 키 쌍 생성
-        let rng = SystemRandom::new();
-
-        // 메시지
-        let message = b"Hello, World!";
-
-        // 서명 생성
-        let mut signature = vec![0; key_pair.public().modulus_len()];
-        key_pair
-            .sign(
-                &ring::signature::RSA_PKCS1_SHA256,
-                &rng,
-                message,
-                &mut signature,
-            )
-            .expect("서명 생성 실패");
-
-        let public_key =
-            ring::signature::UnparsedPublicKey::new(&ring::signature::RSA_PKCS1_2048_8192_SHA256, pkcs8_bytes);
-
-        let result = public_key.verify(message, &signature).is_ok();
-        println!("result = {}", result);
     }
 
     #[test]
--- tests/test.rs
+++ tests/test.rs
@@ -2,16 +2,22 @@
 mod tests {
 
     use sqlx::sqlite::SqlitePoolOptions;
-    use sqlx::{Executor, Row, SqliteConnection};
+    use sqlx::{Column, Executor, FromRow, Row, SqliteConnection};
 
     #[tokio::test]
     async fn sqlx_connection_test() -> sqlx::Result<()> {
 
+        #[derive(Debug, FromRow)]
+        pub struct User {
+            pub id: i64,
+            pub user_id: String,
+            pub name: String,
+            pub passkey: String
+        }
+
         use sqlx::Connection;
 
         // let conn = SqliteConnection::connect("sqlite::memory:").await?;
-
-
 
         let pool = SqlitePoolOptions::new()
             .max_connections(5)
@@ -24,10 +30,29 @@ mod tests {
             .fetch_one(&pool).await?;
 
 
+        sqlx::query("CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id TEXT NOT NULL,
+            name TEXT NOT NULL,
+            passkey TEXT NULLABLE
+        )").execute(&pool).await?;
 
 
+        let cnt = sqlx::query("INSERT INTO users (user_id, name) VALUES ($1, $2)")
+            .bind("user1")
+            .bind("John Doe")
+            .execute(&pool).await?;
 
 
+        println!("cnt.rows_affected() = {:?}", cnt.rows_affected());
+
+        let user = sqlx::query_as::<_, User>("SELECT * FROM users")
+            .fetch_one(&pool).await?;
+        // let user = sqlx::query("SELECT * FROM users")
+        //     .fetch_one(&pool).await?;
+
+
+        println!("user_name = {:?}", user.user_id);
 
         println!("connect!");
 
